c604385d9b745a09e86429e7dbf8a7074c80d4bd
Franco Urbano <frurbano5@gmail.com>
2025-06-12 18:28:21 -0500

    se agregan ejemplos de tipos de datos y expresiones

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,1 +1,24 @@
+#hola
 print("Hola Mundo")
+#tipo enteros
+edad=23
+print(type(edad))
+coordenada=-4
+print(type(coordenada))
+
+#tipos float
+estatura=1.73
+print(type(estatura))
+
+#tipos booleano
+estado=False
+print(type(estado))
+
+#no hay tipos de datos char, solo string
+signo='$'
+valor="%"
+print(type(signo))
+print(type(valor))
+
+mensaje1="cadena con tabulacion \t prueba \n salto"
+print(mensaje1)
